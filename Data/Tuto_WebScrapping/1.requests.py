e1a379c9d9bc8b130e6ad6b1d4a4fc85db2a076d
SangminSHIM <tkdals4723@gmail.com>
2022-10-11 20:10:04 +0200

    let's dive deeper

--- Data/Tuto_WebScrapping/1.requests.py
+++ Data/Tuto_WebScrapping/1.requests.py
@@ -1,7 +1,6 @@
-from subprocess import getstatusoutput
 import requests
 
-class SiteRequests():
+class SiteRequests(requests.Response):
     def __init__(self, name):
         self.name = name
 
@@ -9,24 +8,32 @@ class SiteRequests():
         return f"https://{self.name}.com"
     
     def get_status(self)->int:
-        get_link = requests.get(self.getLink())
+        get_link = requests.get(self.get_link())
         return get_link.status_code
 
     def status_ok(self):
-        if self.getStatus() == requests.codes.ok:
+        if self.get_status() == requests.codes.ok:
             print("Access OK")
         else:
             print("Access Denied")
+    
+# naver = SiteRequests("naver")
+# fbref = SiteRequests("fbref")
 
-naver = SiteRequests("naver")
-fbref = SiteRequests("fbref")
+# print(naver.get_link())
+# print(naver.get_status())
+# print(naver.status_ok())
 
-print(naver.get_link())
-print(naver.get_status())
-print(naver.status_ok())
-
-print(fbref.get_link())
-print(fbref.get_status())
-print(fbref.status_ok())
+# print(fbref.get_link())
+# print(fbref.get_status())
+# print(fbref.status_ok())
 
 # 10/10/2022, 36:28 ~ (raise_for_status())
+
+res = requests.get("https://fbref.com")
+res.raise_for_status()
+print(len(res.text))
+
+with open("fbref.html", "w", encoding="utf8") as file:
+    file.write(res.text)
+    
